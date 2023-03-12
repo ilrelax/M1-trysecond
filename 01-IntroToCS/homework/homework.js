392f8c3ce980b80b90ca1d3269231e8c24b72302
ilrelax <jmauricio.nic@outlook.com>
2023-03-12 11:30:03 -0500

    M1 Henry

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+  console.log("hola")
+}
+
+function DecimalABinario(num) {
+  // tu codigo aca
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
